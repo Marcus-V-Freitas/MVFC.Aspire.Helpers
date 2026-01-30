66b58a186f37f149566c0387c4268e93186ea53b
Marcus Costa <53195917+Marcus-V-Freitas@users.noreply.github.com>
2026-01-30 06:41:00 -0300

    Atualizada versão do xunit.

--- test/MVFC.Aspire.Helpers.Tests/TestUtils/AppHostFixture.cs
+++ test/MVFC.Aspire.Helpers.Tests/TestUtils/AppHostFixture.cs
@@ -7,7 +7,7 @@ public sealed class AppHostFixture : IAsyncLifetime {
 
     public HttpClient HttpClient { get; private set; } = default!;
 
-    public async Task InitializeAsync() {
+    public async ValueTask InitializeAsync() {
         AppHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.MVFC_Aspire_Helpers_Playground_AppHost>();
 
         DistributedApplication = await AppHost.BuildAsync();
@@ -18,7 +18,7 @@ public sealed class AppHostFixture : IAsyncLifetime {
         };
     }
 
-    public async Task DisposeAsync() {
+    public async ValueTask DisposeAsync() {
         await DistributedApplication.DisposeAsync();
         await AppHost.DisposeAsync();
     }
